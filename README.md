a062e293d3715bab7cac94d78c05e1481ea31744
Olivia <oaaole@Olivias-MacBook-Pro-2.local>
2018-08-26 17:18:25 -0700

    final

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # React-Clicky-Pop-Icons
-React Clicky Homework
+Simple game built using react, css, materialize, node, express
+
+# Rules
+Click on the images to play but don't click on the same image twice! 
+
+# Live Link
+https://clickypopicons.herokuapp.com/
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,19 @@ import React from "react";
 import "./Header.css";
 
 const Header = props => (
+
   <div className="header">
-    <div className="title">{props.children}</div>
-    <div className="scores">
-      Score: {props.score} Highscore: {props.highscore}
-    </div>
+    <nav>
+      <div className="header">
+        <div className="title"> Don't click on the same image twice!</div>
+
+        <div className="scores">
+          Score: {props.score} | Highscore: {props.highscore}
+        </div>
+      </div>
+    </nav>
   </div>
-);
+
+)
 
 export default Header;
